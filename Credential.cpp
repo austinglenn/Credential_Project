1e54f1c98d41cb4aad09272c345de6396d5473da
C-Kimber <taylor.kimber@suusuccess.org>
2016-02-24 11:38:33 -0700

    Changed to Sequenctial Access files

--- Credential.cpp
+++ Credential.cpp
@@ -1,39 +1,74 @@
-//Crodential Project
-//2016
-//Clifton Kimber, Jaydon ---, Wiatt Giles, ----- ----,
-
-
 #include <iostream>
-#include <iomanip>
-
+#include <string>
+#include <fstream>
+#include <string.h>
 using namespace std;
 
+void addName();
+void readName(string FULLNAME);
+
+int main ()
+{
+	/*string fullname;
+	
+	cout << "Full name: ";
+	cin >> fullname;
+	
+	readName(fullname);*/
+	addName();
+	
+	 
+
+ return 0;
+}
 
 
-void addApplicant(int applicants[], int &applicant_num );
+void addName(){
+	string line;
+	fstream File("scores.txt");
+	while ( getline (File,line) ){
+		if (line == " "){
+			string FULLNAME; // Student's name
+			float S1, S2, S3;  // Three test scores
+			
 
-int main() {
-	
-	int applicants [] = {0,1,0,0,0,0};
-	
-	int applicant_num = 0;
-	applicant_num = sizeof( applicants ) / sizeof( applicants[ 0 ] );
-	
-	if (applicant_num <= 0){
-		
-		addApplicant(applicants, applicant_num);
-		
+			cout << "Enter the student\'s full name: ";
+			getline(cin,FULLNAME);
+			cout << "Enter three test scores (separated by spaces): ";
+			cin >> S1 >> S2 >> S3;
+
+			cout << "Writing student record to disk..." << endl;
+			File << endl << FULLNAME << ' '<< S1 << ' ' << S2  << ' ' << S3 << endl;
+			
+			File.close();
+			cout << "Student record written to disk file: scores.txt" << endl;
+		}
+		else{
+			cout << "next line..";
+		}
 	}
-	cout << "number of applicants: " << applicant_num;
-	
-	
-	return 0;
 }
 
+void readName(string FULLNAME){
+	string line;
+	float S1, S2, S3;  // Three test scores
+	cout << "Reading student record from disk file: scores.txt" << endl <<endl;
 
-void addApplicant(int applicants[], int &applicant_num ){
+	ifstream inputFile("scores.txt");
+	while ( getline (inputFile,line) )
+	{
+		if (line == FULLNAME){
+			inputFile >> S1 >> S2 >>S3;
+			inputFile.close();
+		}
+	}
+
 	
-	string app_name_ = "";
+
+	cout << "Student\'s name: " << FULLNAME << endl;
+	cout << "Score 1: " << S1 << endl;
+	cout << "Score 2: " << S2 << endl;
+	cout << "Score 3: " << S3 << endl;
 	
-	applicant_num += 1; 
 }
+
